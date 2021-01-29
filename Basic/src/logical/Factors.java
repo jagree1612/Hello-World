d6a106db98cad85f049b1a199dc3c0ce30695643
SAI <SAI@DESKTOP-IP68VSP>
2021-01-30 00:42:50 +0530

    commit-1

---
+++ Basic/src/logical/Factors.java
@@ -0,0 +1,16 @@
+package logical;
+
+public class Factors {
+
+	public static void main(String args[]) {
+		
+		int i,No=9;
+		
+		for(i=1; i<=No; i++) {
+			
+			if (No%i==0)
+				
+				System.out.print(i+ " ");
+			}
+		}
+	}
