95d4a89a72627b1245df5c7ef21b4431fd1f25ad
SAI <SAI@DESKTOP-IP68VSP>
2021-01-30 00:47:05 +0530

    commit-2

---
+++ Basic/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		
+		System.out.println(20+10+"fdgfhdf");
+		
+		System.out.println("dascsa" +20+10);
+	}
+
+}
+
+
+
